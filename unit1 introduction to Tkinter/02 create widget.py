f1fbdf4b384bef98ec4166725685bd57fb844540
MinKhant610 <minkhantiopp@gmail.com>
2022-05-08 22:21:17 +0630

    update

--- unit1 introduction to Tkinter/02 create widget.py
+++ unit1 introduction to Tkinter/02 create widget.py
@@ -2,8 +2,8 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
-lable = ttk.Label(root, text = 'Tkinter Label').pack()
-button = ttk.Button(root, text = 'Click Me') #.pack() or button.pack()
-button.pack () 
+lable = ttk.Label(root,text='Tkinter Label').pack()
+button = ttk.Button(root, text='Click Me') #.pack() or button.pack()
+button.pack()
 #button.config(text = 'Press Me')
 root.mainloop()
